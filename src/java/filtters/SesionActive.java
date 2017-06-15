b0985fcd6e0e15d0bef6f329b9e51f330bc82e74
KervinAraya <arayaromerokervin@gmail.com>
2017-06-14 22:21:36 -0600

    Se termina el modulo de usuarios y se empieza el socio

--- src/java/filtters/SesionActive.java
+++ src/java/filtters/SesionActive.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServletRequest;
  *
  * @author kervin
  */
-@WebFilter(filterName = "SesionActive", urlPatterns = {"/registrarUsuario", "/principal","/eliminarSocio","/eliminarUsuario","/modificarSocio","/modificarUsuario","/registrarSocio","/seleccionarSocio","/seleccionarUsuario","/seleccionarver","/seleccionarverSocios","/verTodosSocios","/verTodosUsuarios","/registrarProducto","/modificarProductos","/verTodosSocios","/eliminarProducto","/vender","/verVentas","/verProductos"})
+@WebFilter(filterName = "SesionActive", urlPatterns = {"/registrarUsuario", "/principal","/eliminarSocio","/eliminarUsuario","/modificarSocio","/modificarUsuario","/registrarSocio","/seleccionarSocio","/seleccionarUsuario","/seleccionarver","/seleccionarverSocios","/verTodosSocios","/verTodosUsuarios","/registrarProducto","/modificarProductos","/verTodosSocios","/eliminarProducto","/verProductos"})
 public class SesionActive implements Filter {
     
     private static final boolean debug = true;
